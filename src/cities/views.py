689a2017b93996c8e16e7ef318faa35e025d1071
markdrrr <markdrrr@gmail.com>
2021-02-03 18:58:56 +0300

    Приложение города

---
+++ src/cities/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+
+from cities.models import City
+
+__all__ = (
+    'home', 'CityDetailView',
+)
+
+
+def home(request):
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
