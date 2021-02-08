49befea3869cc9445d333c1024e940fdd1b289ef
markdrrr <markdrrr@gmail.com>
2021-02-08 16:21:57 +0300

    CRUD trians

--- src/trains/admin.py
+++ src/trains/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from trains.models import Train
 
-# Register your models here.
+
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    list_editable = ('travel_time',)  # 'from_city'
+
+
+admin.site.register(Train, TrainAdmin)
--- src/travel/urls.py
+++ src/travel/urls.py
@@ -6,5 +6,6 @@ from travel.view import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
+    path('trains/', include(('trains.urls', 'trains'))),
     path('', home, name='home'),
 ]
