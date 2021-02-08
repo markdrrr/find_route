3d1c6091e5197e3035541a05539fa18c2893cfa6
markdrrr <markdrrr@gmail.com>
2021-02-08 18:10:30 +0300

    Приложение маршруты

--- src/travel/urls.py
+++ src/travel/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from travel.view import home
+from routes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
