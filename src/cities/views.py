48edeb70d1b4cdf837313b482499d9f4ef7fe6c7
markdrrr <markdrrr@gmail.com>
2021-02-09 15:17:37 +0300

    Приложение accounts

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator
 from django.shortcuts import render
@@ -29,7 +30,7 @@ class CityDetailView(DetailView):
     template_name = 'cities/detail.html'
 
 
-class CityCreateView(SuccessMessageMixin, CreateView):
+class CityCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
@@ -37,7 +38,7 @@ class CityCreateView(SuccessMessageMixin, CreateView):
     success_message = "Город успешно создан"
 
 
-class CityUpdateView(SuccessMessageMixin, UpdateView):
+class CityUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
@@ -45,7 +46,7 @@ class CityUpdateView(SuccessMessageMixin, UpdateView):
     success_message = "Город успешно отредактирован"
 
 
-class CityDeleteView(DeleteView):
+class CityDeleteView(LoginRequiredMixin, DeleteView):
     model = City
     # template_name = 'cities/delete.html'
     success_url = reverse_lazy('cities:home')
