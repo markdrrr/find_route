9e512d4589a17b6283a4b10e30bd3eca351ac7da
markdrrr <markdrrr@gmail.com>
2021-02-09 13:53:07 +0300

    CRUD routes

--- src/travel/settings.py
+++ src/travel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^kq7o*+33j)3#jh%u5ayleo!bz@@x^3#*%-em%8@c6lg2z)&1r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
